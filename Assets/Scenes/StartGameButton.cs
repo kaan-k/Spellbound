af3689d2345e27f66e9ea08c0974de8fd5187cfa
kaan-k <63565880+kaan-k@users.noreply.github.com>
2024-08-29 14:55:50 +0300

    Bug Fixes and Enemy Spawn System.

--- Assets/Scenes/StartGameButton.cs
+++ Assets/Scenes/StartGameButton.cs
@@ -8,6 +8,7 @@ public class StartGameButton : MonoBehaviour
     // This function will be called when the button is clicked
     public void StartGame()
     {
+        Debug.Log("TEST");
         // Replace "YourSceneName" with the actual name of the scene you want to load
         SceneManager.LoadScene("SampleScene");
     }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -8,41 +8,30 @@ public class Room : MonoBehaviour
     public GameObject[] doors;
 
     private bool roomActive;
+    private bool spawnedOnce;
+
+    public Transform enemySpawner;
+    public GameObject enemy;
+    public int enemiesToSpawn;
+
 
     public List<GameObject> enemies = new List<GameObject>();
     void Start()
     {
-        
+        spawnedOnce = false;
+        roomActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enemies.Count > 0 && roomActive && openUponClear)
-        {
-            for(int i = 0; i < enemies.Count; i++)
-            {
-                if(enemies[i] == null)
-                {
-                    enemies.RemoveAt(i);
-                    i--;
-                }
-            }
-            if(enemies.Count == 0)
-            {
-                foreach (GameObject door in doors)
-                {
-                    door.SetActive(false);
-
-                    closeUponEntery = false;
-                }
-            }
-        }
+        RoomLoop();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            EnemySpawner();
             cameraController.instance.ChangeTarget(transform);
 
             if (closeUponEntery)
@@ -53,6 +42,7 @@ public class Room : MonoBehaviour
 
                 }
             }
+            
         }
         roomActive = true;
     }
@@ -61,6 +51,64 @@ public class Room : MonoBehaviour
         if(collision.tag == "Player")
         {
             roomActive = false;
+        }
+    }
+
+    private void EnemySpawner()
+    {
+
+        for (int i = 0;i < enemiesToSpawn; i++)
+        {
+            if (!spawnedOnce)
+            {
+                enemySpawner.localPosition = new Vector3(Random.Range(-6f, 6f), Random.Range(-3f, 3f), 0f);
+
+
+                Vector3 spawnPosition = enemySpawner.position;
+
+                Debug.Log($"Spawning enemy at: {spawnPosition}");
+
+
+                GameObject newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
+
+
+                enemies.Add(newEnemy);
+
+                //enemySpawner.localPosition = new Vector3(0f, 0f, 0f);
+
+                //enemySpawner.localPosition = new Vector3(Random.Range(-6f, 6f), Random.Range(-3f, 3f), 0f);
+                //GameObject newEnemy = Instantiate(enemy, enemySpawner.localPosition, enemySpawner.localRotation, transform);
+                //enemies.Add(newEnemy);
+
+                //Instantiate(enemy, enemySpawner.localPosition, enemySpawner.localRotation);
+            }
+
+        }
+        spawnedOnce = true;
+        
+
+    }
+    private void RoomLoop()
+    {
+        if (enemies.Count > 0 && roomActive && openUponClear)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (enemies.Count == 0 && spawnedOnce)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+
+                    closeUponEntery = false;
+                }
+            }
         }
     }
 }
